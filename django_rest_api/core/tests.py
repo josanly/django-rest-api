fbab233bb9f9ba9ce9c5e8fddf3dd7d73d5528ed
josanly <josso.adrien@gmail.com>
2020-04-18 18:51:44 +0200

    add very basic unit test

--- django_rest_api/core/tests.py
+++ django_rest_api/core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django_rest_api.core.models import Analysis
 
 # Create your tests here.
+class AnalysisTestCase(TestCase):
+    def setUp(self):
+        Analysis.objects.create(name="analysis-test")
+
+    def test_analysis_created_date(self):
+        """Analysis nimals that can speak are correctly identified"""
+        analysis = Analysis.objects.get(name="analysis-test")
+        self.assertEqual(analysis.name, 'analysis-test')
