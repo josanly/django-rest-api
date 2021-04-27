6c6d617aeceb435b069e77fd4aae46eb2ef569b0
josanly <josso.adrien@gmail.com>
2021-04-27 15:05:39 +0200

    heroku deployment

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,6 @@ GRAPH_MODELS = {
   'all_applications': True,
   'group_models': True,
 }
+
+# Heroku deployment (at the end of settings file)
+django_heroku.settings(locals())
