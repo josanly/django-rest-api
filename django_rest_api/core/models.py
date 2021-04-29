1fd8e8d796b92494bda9d96f1bffc39d1e2378bf
josanly <josso.adrien@gmail.com>
2021-04-29 15:56:11 +0200

    Analysis model: comment not mandatory

--- django_rest_api/core/models.py
+++ django_rest_api/core/models.py
@@ -6,7 +6,7 @@ class Analysis(models.Model):
     name    = models.CharField(max_length=100, blank=False, default='AA001')
     sample  = models.CharField(max_length=100, blank=False, default='STR_AA001/1/1')
     created = models.DateTimeField(auto_now_add=True)
-    comment = models.TextField()
+    comment = models.TextField(blank=True)
 
     class Meta:
         ordering = ['created']
